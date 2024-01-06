aafd3b8e0e9f6a51815b3f94aa8352d128a2536a
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-01-06 20:05:24 +0900

    feat: random

---
+++ pkg/random/random_namespace.go
@@ -0,0 +1,21 @@
+package namespace
+
+import (
+	tmrand "github.com/cometbft/cometbft/libs/rand"
+	ns "github.com/sunrise-zone/sunrise-app/pkg/namespace"
+)
+
+func RandomNamespace() ns.Namespace {
+	for {
+		id := RandomVerzionZeroID()
+		namespace, err := ns.New(ns.NamespaceVersionZero, id)
+		if err != nil {
+			continue
+		}
+		return namespace
+	}
+}
+
+func RandomVerzionZeroID() []byte {
+	return append(ns.NamespaceVersionZeroPrefix, tmrand.Bytes(ns.NamespaceVersionZeroIDSize)...)
+}
