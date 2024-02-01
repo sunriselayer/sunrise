51207870fa7e51f0fe26d6083b90458421fce76d
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-02-01 17:08:18 +0900

    chore: codec

--- x/blob/types/codec.go
+++ x/blob/types/codec.go
@@ -1,11 +1,20 @@
 package types
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	// this line is used by starport scaffolding # 1
 )
+
+const (
+	URLMsgPayForBlobs = "/sunrise.blob.v1.MsgPayForBlobs"
+)
+
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgPayForBlobs{}, URLMsgPayForBlobs, nil)
+}
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
--- x/blobstream/types/codec.go
+++ x/blobstream/types/codec.go
@@ -1,11 +1,18 @@
 package types
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	// this line is used by starport scaffolding # 1
 )
+
+const URLMsgRegisterEVMAddress = "/sunrise.blob.v1.MsgRegisterEVMAddress"
+
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgRegisterEvmAddress{}, URLMsgRegisterEVMAddress, nil)
+}
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
