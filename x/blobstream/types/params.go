1225f88de3cebc981c111f4066c0109a9fc6d434
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-02-09 17:55:14 +0900

    chore: func Validate()

--- x/blobstream/types/params.go
+++ x/blobstream/types/params.go
@@ -7,6 +7,11 @@ import (
 
 var _ paramtypes.ParamSet = (*Params)(nil)
 
+// ParamKeyTable the param key table for blobstream module
+func ParamKeyTable() paramtypes.KeyTable {
+	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
+}
+
 // NewParams creates a new Params instance
 func NewParams() Params {
 	return Params{}
@@ -17,21 +22,15 @@ func DefaultParams() Params {
 	return NewParams()
 }
 
-// ParamKeyTable for blobstream module
-func ParamKeyTable() paramtypes.KeyTable {
-	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
-}
-
-// ParamSetPairs implements the ParamSet interface and returns all the key/value
-// pairs of auth module's parameters.
+// ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
 	return paramtypes.ParamSetPairs{
 		paramtypes.NewParamSetPair(ParamsStoreKeyDataCommitmentWindow, &p.DataCommitmentWindow, validateDataCommitmentWindow),
 	}
 }
 
-// ValidateBasic checks that the parameters have valid values.
-func (p Params) ValidateBasic() error {
+// Validate validates the set of params
+func (p Params) Validate() error {
 	if err := validateDataCommitmentWindow(p.DataCommitmentWindow); err != nil {
 		return errors.Wrap(err, "data commitment window")
 	}
