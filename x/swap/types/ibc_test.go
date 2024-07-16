02c114d9b7b7469328c22e3b92919046e96791d5
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-07-16 21:34:32 +0900

    feat: error in series

--- x/swap/types/ibc_test.go
+++ x/swap/types/ibc_test.go
@@ -47,6 +47,59 @@ func TestEncodePacketMetadata_ExactAmountIn(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func TestEncodePacketMetadata_ExactAmountInSeries(t *testing.T) {
+	retries := uint8(2)
+	packetMetadata := PacketMetadata{
+		Swap: &SwapMetadata{
+			InterfaceProvider: "sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y",
+			Route: SwapRoute{
+				DenomIn:  "tokenIn",
+				DenomOut: "tokenOut2",
+				Strategy: SwapStrategy{
+					Series: &RouteSeries{
+						Routes: []Route{{
+							DenomIn:  "tokenIn",
+							DenomOut: "tokenOut",
+							Strategy: &Route_Pool{
+								Pool: &RoutePool{
+									PoolId: 1,
+								},
+							}},
+							{
+								DenomIn:  "tokenOut",
+								DenomOut: "tokenOut2",
+								Strategy: &Route_Pool{
+									Pool: &RoutePool{
+										PoolId: 2,
+									},
+								}},
+						},
+					}},
+			},
+			ExactAmountIn: &ExactAmountIn{
+				MinAmountOut: sdkmath.OneInt(),
+			},
+			ExactAmountOut: nil,
+			Forward: &packetforwardtypes.ForwardMetadata{
+				Receiver: "cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9",
+				Port:     "transfer",
+				Channel:  "channel-2",
+				Retries:  &retries,
+				Next:     nil,
+			},
+		},
+	}
+
+	metadataJson, err := json.Marshal(packetMetadata)
+	require.NoError(t, err)
+
+	require.Equal(t, string(metadataJson), `{"swap":{"interface_provider":"sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y","route":{"denom_in":"tokenIn","denom_out":"tokenOut2","strategy":{"series":{"routes":[{"denom_in":"tokenIn","denom_out":"tokenOut","Strategy":{"pool":{"pool_id":1}}},{"denom_in":"tokenOut","denom_out":"tokenOut2","Strategy":{"pool":{"pool_id":2}}}]}}},"exact_amount_in":{"min_amount_out":"1"},"forward":{"receiver":"cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9","port":"transfer","channel":"channel-2","retries":2}}}`)
+
+	metadata := PacketMetadata{}
+	err = json.Unmarshal(metadataJson, &metadata)
+	require.NoError(t, err)
+}
+
 func TestEncodePacketMetadata_ExactAmountOut(t *testing.T) {
 	retries := uint8(2)
 	packetMetadata := PacketMetadata{
