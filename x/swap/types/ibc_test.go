0bd633577354c8129ebf48dae2a879cd2a39911b
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-07-17 18:27:56 +0900

    feat: test

--- x/swap/types/ibc_test.go
+++ x/swap/types/ibc_test.go
@@ -1,23 +1,23 @@
 package types
 
 import (
-	"encoding/json"
+	"strings"
 	"testing"
 
 	sdkmath "cosmossdk.io/math"
-	packetforwardtypes "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v8/packetforward/types"
+	"github.com/gogo/protobuf/jsonpb"
 	"github.com/stretchr/testify/require"
 )
 
 func TestEncodePacketMetadata_ExactAmountIn(t *testing.T) {
-	retries := uint8(2)
+	retries := uint32(2)
 	packetMetadata := PacketMetadata{
 		Swap: &SwapMetadata{
 			InterfaceProvider: "sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y",
-			Route: SwapRoute{
+			Route: &Route{
 				DenomIn:  "tokenIn",
 				DenomOut: "tokenOut",
-				Strategy: SwapStrategy{
+				Strategy: &Route_Pool{
 					Pool: &RoutePool{
 						PoolId: 1,
 					},
@@ -27,35 +27,91 @@ func TestEncodePacketMetadata_ExactAmountIn(t *testing.T) {
 				MinAmountOut: sdkmath.OneInt(),
 			},
 			ExactAmountOut: nil,
-			Forward: &packetforwardtypes.ForwardMetadata{
+			Forward: &ForwardMetadata{
 				Receiver: "cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9",
 				Port:     "transfer",
 				Channel:  "channel-2",
-				Retries:  &retries,
+				Retries:  retries,
 				Next:     nil,
 			},
 		},
 	}
 
-	metadataJson, err := json.Marshal(packetMetadata)
+	m := jsonpb.Marshaler{}
+	js, err := m.MarshalToString(&packetMetadata)
 	require.NoError(t, err)
 
-	require.Equal(t, string(metadataJson), `{"swap":{"interface_provider":"sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y","route":{"denom_in":"tokenIn","denom_out":"tokenOut","strategy":{"pool":{"pool_id":1}}},"exact_amount_in":{"min_amount_out":"1"},"forward":{"receiver":"cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9","port":"transfer","channel":"channel-2","retries":2}}}`)
+	require.Equal(t, js, `{"swap":{"interfaceProvider":"sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y","route":{"denomIn":"tokenIn","denomOut":"tokenOut","pool":{"poolId":"1"}},"exactAmountIn":{"minAmountOut":"1"},"forward":{"receiver":"cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9","port":"transfer","channel":"channel-2","retries":2}}}`)
 
-	metadata := PacketMetadata{}
-	err = json.Unmarshal(metadataJson, &metadata)
+	metadata := &PacketMetadata{}
+	err = jsonpb.Unmarshal(strings.NewReader(js), metadata)
+	require.NoError(t, err)
+}
+
+func TestEncodePacketMetadata_ExactAmountInSeries(t *testing.T) {
+	retries := uint32(2)
+	packetMetadata := PacketMetadata{
+		Swap: &SwapMetadata{
+			InterfaceProvider: "sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y",
+			Route: &Route{
+				DenomIn:  "tokenIn",
+				DenomOut: "tokenOut",
+				Strategy: &Route_Series{
+					Series: &RouteSeries{
+						Routes: []Route{{
+							DenomIn:  "tokenIn",
+							DenomOut: "tokenOut",
+							Strategy: &Route_Pool{
+								Pool: &RoutePool{
+									PoolId: 1,
+								},
+							}},
+							{
+								DenomIn:  "tokenOut",
+								DenomOut: "tokenOut2",
+								Strategy: &Route_Pool{
+									Pool: &RoutePool{
+										PoolId: 2,
+									},
+								}},
+						},
+					},
+				},
+			},
+			ExactAmountIn: &ExactAmountIn{
+				MinAmountOut: sdkmath.OneInt(),
+			},
+			ExactAmountOut: nil,
+			Forward: &ForwardMetadata{
+				Receiver: "cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9",
+				Port:     "transfer",
+				Channel:  "channel-2",
+				Retries:  retries,
+				Next:     nil,
+			},
+		},
+	}
+
+	m := jsonpb.Marshaler{}
+	js, err := m.MarshalToString(&packetMetadata)
+	require.NoError(t, err)
+
+	require.Equal(t, js, `{"swap":{"interfaceProvider":"sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y","route":{"denomIn":"tokenIn","denomOut":"tokenOut","series":{"routes":[{"denomIn":"tokenIn","denomOut":"tokenOut","pool":{"poolId":"1"}},{"denomIn":"tokenOut","denomOut":"tokenOut2","pool":{"poolId":"2"}}]}},"exactAmountIn":{"minAmountOut":"1"},"forward":{"receiver":"cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9","port":"transfer","channel":"channel-2","retries":2}}}`)
+
+	metadata := &PacketMetadata{}
+	err = jsonpb.Unmarshal(strings.NewReader(js), metadata)
 	require.NoError(t, err)
 }
 
 func TestEncodePacketMetadata_ExactAmountOut(t *testing.T) {
-	retries := uint8(2)
+	retries := uint32(2)
 	packetMetadata := PacketMetadata{
 		Swap: &SwapMetadata{
 			InterfaceProvider: "sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y",
-			Route: SwapRoute{
+			Route: &Route{
 				DenomIn:  "tokenIn",
 				DenomOut: "tokenOut",
-				Strategy: SwapStrategy{
+				Strategy: &Route_Pool{
 					Pool: &RoutePool{
 						PoolId: 1,
 					},
@@ -64,30 +120,31 @@ func TestEncodePacketMetadata_ExactAmountOut(t *testing.T) {
 			ExactAmountIn: nil,
 			ExactAmountOut: &ExactAmountOut{
 				AmountOut: sdkmath.NewInt(1000),
-				Change: &packetforwardtypes.ForwardMetadata{
+				Change: &ForwardMetadata{
 					Receiver: "cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9",
 					Port:     "transfer",
 					Channel:  "channel-2",
-					Retries:  &retries,
+					Retries:  retries,
 					Next:     nil,
 				},
 			},
-			Forward: &packetforwardtypes.ForwardMetadata{
+			Forward: &ForwardMetadata{
 				Receiver: "cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9",
 				Port:     "transfer",
 				Channel:  "channel-2",
-				Retries:  &retries,
+				Retries:  retries,
 				Next:     nil,
 			},
 		},
 	}
 
-	metadataJson, err := json.Marshal(packetMetadata)
+	m := jsonpb.Marshaler{}
+	js, err := m.MarshalToString(&packetMetadata)
 	require.NoError(t, err)
 
-	require.Equal(t, string(metadataJson), `{"swap":{"interface_provider":"sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y","route":{"denom_in":"tokenIn","denom_out":"tokenOut","strategy":{"pool":{"pool_id":1}}},"exact_amount_out":{"amount_out":"1000","change":{"receiver":"cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9","port":"transfer","channel":"channel-2","retries":2}},"forward":{"receiver":"cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9","port":"transfer","channel":"channel-2","retries":2}}}`)
+	require.Equal(t, js, `{"swap":{"interfaceProvider":"sunrise18atdu5vvsg95sdpvdwsv7kevlzg8jhtuk7hs4y","route":{"denomIn":"tokenIn","denomOut":"tokenOut","pool":{"poolId":"1"}},"exactAmountOut":{"amountOut":"1000","change":{"receiver":"cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9","port":"transfer","channel":"channel-2","retries":2}},"forward":{"receiver":"cosmos1qnk2n4nlkpw9xfqntladh74w6ujtulwn7j8za9","port":"transfer","channel":"channel-2","retries":2}}}`)
 
-	metadata := PacketMetadata{}
-	err = json.Unmarshal(metadataJson, &metadata)
+	metadata := &PacketMetadata{}
+	err = jsonpb.Unmarshal(strings.NewReader(js), metadata)
 	require.NoError(t, err)
 }
