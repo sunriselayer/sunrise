42a8a28212b156b144968af75cab96054118e707
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-10-08 20:17:43 +0900

    feat; store upgrade

--- app/upgrades/v0.3.0-test/constants.go
+++ app/upgrades/v0.3.0-test/constants.go
@@ -2,17 +2,17 @@ package v0_3_0_test
 
 import (
 	storetypes "cosmossdk.io/store/types"
-
 	"github.com/sunriselayer/sunrise/app/upgrades"
+	datypes "github.com/sunriselayer/sunrise/x/da/types"
 )
 
-const UpgradeName string = "v0_2_2_test"
+const UpgradeName string = "v0_3_0_test"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
 	StoreUpgrades: storetypes.StoreUpgrades{
-		Added:   []string{},
-		Deleted: []string{},
+		Added:   []string{datypes.StoreKey},
+		Deleted: []string{datypes.StoreKey},
 	},
 }
