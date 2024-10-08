616a966c958b081fba3065147c816f032b1ea479
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-10-08 15:10:27 +0900

    fix: other tests

--- test/util/genesis/modifier.go
+++ test/util/genesis/modifier.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"time"
 
-	blobtypes "github.com/sunriselayer/sunrise/x/blob/types"
-	bstypes "github.com/sunriselayer/sunrise/x/blobstream/types"
+	// blobtypes "github.com/sunriselayer/sunrise/x/blob/types"
+	// bstypes "github.com/sunriselayer/sunrise/x/blobstream/types"
 
 	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -23,15 +23,15 @@ import (
 // and is expected to return the modified genesis as output.
 type Modifier func(state map[string]json.RawMessage) map[string]json.RawMessage
 
-// SetBlobParams will set the provided blob params as genesis state.
-func SetBlobParams(codec codec.Codec, params blobtypes.Params) Modifier {
-	return func(state map[string]json.RawMessage) map[string]json.RawMessage {
-		blobGenState := blobtypes.DefaultGenesis()
-		blobGenState.Params = params
-		state[blobtypes.ModuleName] = codec.MustMarshalJSON(blobGenState)
-		return state
-	}
-}
+// // SetBlobParams will set the provided blob params as genesis state.
+// func SetBlobParams(codec codec.Codec, params blobtypes.Params) Modifier {
+// 	return func(state map[string]json.RawMessage) map[string]json.RawMessage {
+// 		blobGenState := blobtypes.DefaultGenesis()
+// 		blobGenState.Params = params
+// 		state[blobtypes.ModuleName] = codec.MustMarshalJSON(blobGenState)
+// 		return state
+// 	}
+// }
 
 // ImmediateProposals sets the thresholds for getting a gov proposal to very low
 // levels.
@@ -48,16 +48,16 @@ func ImmediateProposals(codec codec.Codec) Modifier {
 	}
 }
 
-// SetDataCommitmentWindow will set the provided data commitment window in the
-// blobstream module's genesis state.
-func SetDataCommitmentWindow(codec codec.Codec, window uint64) Modifier {
-	return func(state map[string]json.RawMessage) map[string]json.RawMessage {
-		blobstreamGenState := bstypes.DefaultGenesis()
-		blobstreamGenState.Params.DataCommitmentWindow = window
-		state[bstypes.ModuleName] = codec.MustMarshalJSON(blobstreamGenState)
-		return state
-	}
-}
+// // SetDataCommitmentWindow will set the provided data commitment window in the
+// // blobstream module's genesis state.
+// func SetDataCommitmentWindow(codec codec.Codec, window uint64) Modifier {
+// 	return func(state map[string]json.RawMessage) map[string]json.RawMessage {
+// 		blobstreamGenState := bstypes.DefaultGenesis()
+// 		blobstreamGenState.Params.DataCommitmentWindow = window
+// 		state[bstypes.ModuleName] = codec.MustMarshalJSON(blobstreamGenState)
+// 		return state
+// 	}
+// }
 
 // FundAccounts adds a set of accounts to the genesis and then sets their balance as provided.
 // This is good in the case where you have a separate keyring you want to test against and not
