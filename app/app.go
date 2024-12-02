99c4a3e6b86b45b4e036311c43632b9ac2b4528c
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2024-12-02 16:38:01 +0800

    ante handler

--- app/app.go
+++ app/app.go
@@ -60,7 +60,6 @@ import (
 	"github.com/skip-mev/block-sdk/v2/block"
 	"github.com/skip-mev/block-sdk/v2/block/base"
 	"github.com/skip-mev/block-sdk/v2/block/service"
-	mevlane "github.com/skip-mev/block-sdk/v2/lanes/mev"
 	auctionkeeper "github.com/skip-mev/block-sdk/v2/x/auction/keeper"
 	"github.com/sunriselayer/sunrise/app/ante"
 	"github.com/sunriselayer/sunrise/app/keepers"
@@ -77,8 +76,6 @@ import (
 	feetypes "github.com/sunriselayer/sunrise/x/fee/types"
 	tokenconvertertypes "github.com/sunriselayer/sunrise/x/tokenconverter/types"
 
-	// blobmodulekeeper "github.com/sunriselayer/sunrise/x/blob/keeper"
-	// streammodulekeeper "github.com/sunriselayer/sunrise/x/blobstream/keeper"
 	damodulekeeper "github.com/sunriselayer/sunrise/x/da/keeper"
 	feemodulekeeper "github.com/sunriselayer/sunrise/x/fee/keeper"
 	liquidityincentivemodulekeeper "github.com/sunriselayer/sunrise/x/liquidityincentive/keeper"
@@ -176,7 +173,6 @@ type App struct {
 	sm *module.SimulationManager
 
 	// custom structure for skip-mev protection
-	MevLane        *mevlane.MEVLane
 	CheckTxHandler checktx.CheckTx
 }
 
@@ -417,7 +413,6 @@ func New(
 		app.AccountKeeper,
 		app.BankKeeper,
 		app.FeeGrantKeeper,
-		// app.BlobKeeper,
 		app.FeeKeeper,
 		app.txConfig.SignModeHandler(),
 		ante.DefaultSigVerificationGasConsumer,
@@ -441,8 +436,6 @@ func New(
 		opt...,
 	)
 
-	app.MevLane = mevLane
-
 	// Step 6: Create the proposal handler and set it on the app. Now the application
 	// will build and verify proposals using the Block SDK!
 	blockSdkProposalHandler := abci.NewDefaultProposalHandler(
@@ -452,16 +445,16 @@ func New(
 		mempool,
 	)
 
-	propHandler := NewProposalHandler(
+	daProposalHandler := NewProposalHandler(
 		logger,
 		app.DaKeeper,
 		app.StakingKeeper,
 		app.ModuleManager,
 		blockSdkProposalHandler,
 	)
-	app.BaseApp.SetPrepareProposal(propHandler.PrepareProposal())
-	app.BaseApp.SetProcessProposal(propHandler.ProcessProposal())
-	app.BaseApp.SetPreBlocker(propHandler.PreBlocker)
+	app.BaseApp.SetPrepareProposal(daProposalHandler.PrepareProposal())
+	app.BaseApp.SetProcessProposal(daProposalHandler.ProcessProposal())
+	app.BaseApp.SetPreBlocker(daProposalHandler.PreBlocker)
 
 	// Step 7: Set the custom CheckTx handler on BaseApp. This is only required if you
 	// use the MEV lane.
@@ -472,26 +465,29 @@ func New(
 		anteHandler,
 		app.App.CheckTx,
 	)
-	checkTxHandler := checktx.NewMempoolParityCheckTx(
+	parityCheckTx := checktx.NewMempoolParityCheckTx(
 		app.Logger(), mempool,
 		app.txConfig.TxDecoder(),
 		mevCheckTx.CheckTx(),
 		app.BaseApp,
 	)
 
-	app.SetCheckTx(checkTxHandler.CheckTx())
-
-	// <sunrise>
-	// Step 8: Set the custom Upgrade handler on BaseApp. This is added for on-chain upgrade.
-	app.setupUpgradeHandlers()
-	// Step 9: Set the custom upgrade store loaders on BaseApp.
-	app.setupUpgradeStoreLoaders()
-	// </sunrise>
+	app.SetCheckTx(parityCheckTx.CheckTx())
+	app.SetAnteHandler(anteHandler)
 
 	// ---------------------------------------------------------------------------- //
 	// ------------------------- End `Skip MEV` Code ------------------------------ //
 	// ---------------------------------------------------------------------------- //
 
+	// <sunrise>
+	// Upgrade Handler
+	// Set the custom Upgrade handler on BaseApp. This is added for on-chain upgrade.
+	app.setupUpgradeHandlers()
+	// Set the custom upgrade store loaders on BaseApp.
+	app.setupUpgradeStoreLoaders()
+	// </sunrise>
+
+	// <sunrise>
 	// Vote extension
 	voteExtHandler := NewVoteExtHandler(app.DaKeeper, app.StakingKeeper)
 
@@ -502,6 +498,7 @@ func New(
 
 	app.App.BaseApp.SetExtendVoteHandler(voteExtHandler.ExtendVoteHandler(daConfig, app.txConfig.TxDecoder(), anteHandler, app.DaKeeper))
 	app.App.BaseApp.SetVerifyVoteExtensionHandler(voteExtHandler.VerifyVoteExtensionHandler(daConfig, app.DaKeeper))
+	// </sunrise>
 
 	app.ModuleManager.RegisterInvariants(app.CrisisKeeper)
 
@@ -660,11 +657,6 @@ func BlockedAddresses() map[string]bool {
 		}
 	}
 	return result
-}
-
-// GetTxConfig implements the TestingApp interface.
-func (app *App) GetTxConfig() client.TxConfig {
-	return app.txConfig
 }
 
 // <sunrise>
