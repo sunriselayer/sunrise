71337a0a53967ddd4112bb74c5330c313aad1e76
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-01-15 15:00:28 +0900

    feat: proxy msg

--- x/tokenconverter/types/codec.go
+++ x/tokenconverter/types/codec.go
@@ -5,7 +5,8 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
-	v1 "github.com/sunriselayer/sunrise/x/accounts/self_delegatable_lockup/v1"
+	lockuptypes "github.com/sunriselayer/sunrise/x/accounts/self_delegatable_lockup/v1"
+	proxytypes "github.com/sunriselayer/sunrise/x/accounts/self_delegation_proxy/v1"
 )
 
 func RegisterInterfaces(registrar registry.InterfaceRegistrar) {
@@ -17,9 +18,19 @@ func RegisterInterfaces(registrar registry.InterfaceRegistrar) {
 
 	// TEMP: sunrise.accounts.self_delegatable_lockup.v1
 	registrar.RegisterImplementations((*sdk.Msg)(nil),
-		&v1.MsgSelfDelegate{},
-		&v1.MsgWithdrawSelfDelegationUnbonded{},
-		&v1.MsgSend{},
+		&lockuptypes.MsgInitSelfDelegatableLockupAccount{},
+		&lockuptypes.MsgInitSelfDelegatableLockupAccountResponse{},
+		&lockuptypes.MsgSelfDelegate{},
+		&lockuptypes.MsgWithdrawSelfDelegationUnbonded{},
+		&lockuptypes.MsgSend{},
+	)
+
+	// TEMP: sunrise.accounts.self_delegation_proxy.v1
+	registrar.RegisterImplementations((*sdk.Msg)(nil),
+		&proxytypes.MsgInit{},
+		&proxytypes.MsgUndelegate{},
+		&proxytypes.MsgWithdrawReward{},
+		&proxytypes.MsgSend{},
 	)
 
 	registrar.RegisterImplementations((*sdk.Msg)(nil),
