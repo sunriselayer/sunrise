e2bc6cd6c84b95dc34737f4024c1b5ce71d840d5
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-01-28 02:09:21 +0900

    fix: 2 types

--- x/da/keeper/store_published_data.go
+++ x/da/keeper/store_published_data.go
@@ -62,7 +62,6 @@ func (k Keeper) GetAllPublishedData(ctx context.Context) (list []types.Published
 
 func (k Keeper) GetUnverifiedDataBeforeTime(ctx sdk.Context, timestamp int64) (list []types.PublishedData, err error) {
 	statuses := []string{
-		types.Status_STATUS_MSG_SERVER.String(),
 		types.Status_STATUS_VOTE_EXTENSION.String(),
 		types.Status_STATUS_CHALLENGE_FOR_FRAUD.String(),
 	}
