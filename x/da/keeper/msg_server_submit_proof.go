a204dfd3812d39609e7aba7044b88eac7ee30aa2
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-01-29 16:54:30 +0900

    fix: delete IsValidData

--- x/da/keeper/msg_server_submit_proof.go
+++ x/da/keeper/msg_server_submit_proof.go
@@ -44,44 +44,43 @@ func (k msgServer) SubmitProof(ctx context.Context, msg *types.MsgSubmitProof) (
 	}
 
 	// check proof
-	if msg.IsValidData {
-		// TODO: check number of proofs (threshold)
-		vk, err := zkp.UnmarshalVerifyingKey(params.ZkpVerifyingKey)
+	// TODO: check number of proofs (threshold)
+	vk, err := zkp.UnmarshalVerifyingKey(params.ZkpVerifyingKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// groth16: Prove & Verify
+	for i, j := range msg.Indices {
+		proof := &groth16bn254.Proof{}
+		_, err := proof.ReadFrom(bytes.NewReader(msg.Proofs[i]))
 		if err != nil {
 			return nil, err
 		}
 
-		// groth16: Prove & Verify
-		for i, j := range msg.Indices {
-			proof := &groth16bn254.Proof{}
-			_, err := proof.ReadFrom(bytes.NewReader(msg.Proofs[i]))
-			if err != nil {
-				return nil, err
-			}
-
-			if len(publishedData.ShardDoubleHashes) <= int(j) {
-				return nil, types.ErrProofIndiceOverflow
-			}
-
-			assignment := zkp.ValidityProofCircuit{
-				ShardHash:       big.NewInt(1),
-				ShardDoubleHash: publishedData.ShardDoubleHashes[j],
-			}
-			witness, err := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
-			if err != nil {
-				return nil, err
-			}
-
-			pubWitness, err := witness.Public()
-			if err != nil {
-				return nil, err
-			}
-
-			err = groth16.Verify(proof, vk, pubWitness)
-			if err != nil {
-				return nil, err
-			}
+		if len(publishedData.ShardDoubleHashes) <= int(j) {
+			return nil, types.ErrProofIndiceOverflow
 		}
+
+		assignment := zkp.ValidityProofCircuit{
+			ShardHash:       big.NewInt(1),
+			ShardDoubleHash: publishedData.ShardDoubleHashes[j],
+		}
+		witness, err := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
+		if err != nil {
+			return nil, err
+		}
+
+		pubWitness, err := witness.Public()
+		if err != nil {
+			return nil, err
+		}
+
+		err = groth16.Verify(proof, vk, pubWitness)
+		if err != nil {
+			return nil, err
+		}
+
 	}
 
 	// save proof in the storage
@@ -90,7 +89,6 @@ func (k msgServer) SubmitProof(ctx context.Context, msg *types.MsgSubmitProof) (
 		Sender:      msg.Sender,
 		Indices:     msg.Indices,
 		Proofs:      msg.Proofs,
-		IsValidData: msg.IsValidData,
 	})
 	if err != nil {
 		return nil, err
--- x/da/keeper/msg_server_submit_proof_test.go
+++ x/da/keeper/msg_server_submit_proof_test.go
@@ -80,13 +80,12 @@ func TestMsgSubmitProof(t *testing.T) {
 		expErrMsg string
 	}{
 		{
-			name: "invalid data hash",
+			name: "empty proof",
 			input: &types.MsgSubmitProof{
 				Sender:      sample.AccAddress(),
 				MetadataUri: "ipfs://metadata1",
 				Indices:     []int64{},
 				Proofs:      [][]byte{},
-				IsValidData: false,
 			},
 			expErr: false,
 		},
@@ -97,7 +96,6 @@ func TestMsgSubmitProof(t *testing.T) {
 				MetadataUri: "ipfs://metadata1",
 				Indices:     []int64{0},
 				Proofs:      [][]byte{proofBytes},
-				IsValidData: true,
 			},
 			expErr: false,
 		},
@@ -108,7 +106,6 @@ func TestMsgSubmitProof(t *testing.T) {
 				MetadataUri: "ipfs://metadata1",
 				Indices:     []int64{0},
 				Proofs:      [][]byte{{0x0}},
-				IsValidData: true,
 			},
 			expErr:    true,
 			expErrMsg: "unexpected EOF",
