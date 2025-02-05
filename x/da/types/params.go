9b2c847296a347900977d98eb9ebe6a5181b32da
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-02-05 23:20:49 +0900

    feat: params

--- x/da/types/params.go
+++ x/da/types/params.go
@@ -14,71 +14,58 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
+	"github.com/sunriselayer/sunrise/app/consts"
 	"github.com/sunriselayer/sunrise/x/da/zkp"
 )
 
 // NewParams creates a new Params instance.
 func NewParams(
-	voteThreshold math.LegacyDec,
+	challengeThreshold math.LegacyDec,
+	replicationFactor math.LegacyDec,
 	slashEpoch uint64,
 	epochMaxFault uint64,
 	slashFraction math.LegacyDec,
-	replicationFactor math.LegacyDec,
-	minShardCount uint64,
-	maxShardCount uint64,
-	maxShardSize uint64,
 	challengePeriod time.Duration,
 	proofPeriod time.Duration,
-	challengeCollateral sdk.Coins,
-	zkpProvingKey []byte,
-	zkpVerifyingKey []byte,
 	rejectedRemovalPeriod time.Duration,
+	publishDataCollateral sdk.Coins,
+	submitInvalidityCollateral sdk.Coins,
+	zkpVerifyingKey []byte,
 ) Params {
 	return Params{
-		VoteThreshold:         voteThreshold.String(),
-		SlashEpoch:            slashEpoch,
-		EpochMaxFault:         epochMaxFault,
-		SlashFraction:         slashFraction.String(),
-		ReplicationFactor:     replicationFactor.String(),
-		MinShardCount:         minShardCount,
-		MaxShardCount:         maxShardCount,
-		MaxShardSize:          maxShardSize,
-		ChallengePeriod:       challengePeriod,
-		ProofPeriod:           proofPeriod,
-		ChallengeCollateral:   challengeCollateral,
-		ZkpProvingKey:         zkpProvingKey,
-		ZkpVerifyingKey:       zkpVerifyingKey,
-		RejectedRemovalPeriod: rejectedRemovalPeriod,
+		ChallengeThreshold:         challengeThreshold.String(),
+		ReplicationFactor:          replicationFactor.String(),
+		SlashEpoch:                 slashEpoch,
+		EpochMaxFault:              epochMaxFault,
+		SlashFraction:              slashFraction.String(),
+		ChallengePeriod:            challengePeriod,
+		ProofPeriod:                proofPeriod,
+		RejectedRemovalPeriod:      rejectedRemovalPeriod,
+		PublishDataCollateral:      publishDataCollateral,
+		SubmitInvalidityCollateral: submitInvalidityCollateral,
+		ZkpVerifyingKey:            zkpVerifyingKey,
 	}
 }
 
 // DefaultParams returns a default set of parameters.
 func DefaultParams() Params {
-	provingKey, err := base64.StdEncoding.DecodeString(DefaultProvingKeyBase64)
-	if err != nil {
-		panic(err)
-	}
-
 	verifyingKey, err := base64.StdEncoding.DecodeString(DefaultVerifyingKeyBase64)
 	if err != nil {
 		panic(err)
 	}
 
 	return NewParams(
-		math.LegacyNewDecWithPrec(67, 2), // 67%
-		120960,                           // 1 week
-		34560,                            // 2 days
-		math.LegacyNewDecWithPrec(1, 3),  // 0.1%
-		math.LegacyNewDec(5),             // 5.0
-		10,
-		255,
-		1000000,       // 1MB
-		time.Minute*6, // 6min,
-		time.Minute*8, // 8min
-		sdk.Coins(nil),
-		provingKey,
+		math.LegacyNewDecWithPrec(33, 2), // 33% challenge threshold
+		math.LegacyNewDec(5),             // 5.0 replication factor
+		120960,                           // 1 week(5sec/block) slash epoch
+		100,                              // 100 max fault
+		math.LegacyNewDecWithPrec(1, 3),  // 0.1% slash fraction
+		time.Minute*4,                    // challenge 4min,
+		time.Minute*10,                   // proof 10min
+		time.Hour*24,                     // remove 24h
+		sdk.NewCoins(sdk.NewCoin(consts.FeeDenom, math.NewInt(1_000_000_000))), // publish data collateral 1000RISE
+		sdk.NewCoins(sdk.NewCoin(consts.FeeDenom, math.NewInt(100_000_000))),   // submit invalidity collateral 100RISE
 		verifyingKey,
-		time.Hour*24, // 24h
 	)
 }
 
@@ -110,15 +97,23 @@ func GenerateZkpKeys() (string, string) {
 
 // Validate validates the set of params.
 func (p Params) Validate() error {
-	voteThreshold, err := math.LegacyNewDecFromStr(p.VoteThreshold)
+	challengeThreshold, err := math.LegacyNewDecFromStr(p.ChallengeThreshold)
 	if err != nil {
 		return err
 	}
-	if voteThreshold.IsNegative() {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "vote threshold must not be negative")
+	if challengeThreshold.IsNegative() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "challenge threshold must not be negative")
 	}
-	if voteThreshold.GT(math.LegacyOneDec()) {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "vote threshold must be less than 1")
+	if challengeThreshold.GT(math.LegacyOneDec()) {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "challenge threshold must be less than 1")
+	}
+
+	replicationFactor, err := math.LegacyNewDecFromStr(p.ReplicationFactor)
+	if err != nil {
+		return err
+	}
+	if !replicationFactor.IsPositive() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "replication factor must be negative")
 	}
 
 	if p.SlashEpoch == 0 {
@@ -140,44 +135,21 @@ func (p Params) Validate() error {
 		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "slash fraction must be less than 1")
 	}
 
-	replicationFactor, err := math.LegacyNewDecFromStr(p.ReplicationFactor)
-	if err != nil {
-		return err
-	}
-	if !replicationFactor.IsPositive() {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "replication factor must be negative")
-	}
-
-	if p.MinShardCount == 0 {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "min shard count must be positive")
-	}
-
-	if p.MaxShardCount == 0 {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "max shard count must be positive")
-	}
-
-	if p.MaxShardCount < p.MinShardCount {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "max shard count must be greater than or equal to min shard count")
-	}
-
-	if p.MaxShardSize == 0 {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "max shard size must be positive")
-	}
-
 	if p.ChallengePeriod == 0 {
 		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "challenge period must be positive")
 	}
-
 	if p.ProofPeriod == 0 {
 		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "proof period must be positive")
 	}
-
-	if !p.ChallengeCollateral.IsValid() {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "challenge collateral must be valid")
+	if p.RejectedRemovalPeriod == 0 {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "rejected removal period must be positive")
 	}
 
-	if len(p.ZkpProvingKey) == 0 {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "zkp proving key must not be empty")
+	if !p.PublishDataCollateral.IsValid() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "publish data collateral must be valid")
+	}
+	if !p.SubmitInvalidityCollateral.IsValid() {
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "submit invalidity collateral must be valid")
 	}
 
 	if len(p.ZkpVerifyingKey) == 0 {
