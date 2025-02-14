ee0f9e15379a3102ec709013b9795aaef30c0d30
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-02-14 21:34:35 +0900

    feat: query proto

--- x/da/types/query.pb.gw.go
+++ x/da/types/query.pb.gw.go
@@ -182,7 +182,7 @@ var (
 )
 
 func request_Query_Invalidity_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryValidityProofRequest
+	var protoReq QueryInvalidityRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
@@ -198,7 +198,7 @@ func request_Query_Invalidity_0(ctx context.Context, marshaler runtime.Marshaler
 }
 
 func local_request_Query_Invalidity_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryValidityProofRequest
+	var protoReq QueryInvalidityRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
