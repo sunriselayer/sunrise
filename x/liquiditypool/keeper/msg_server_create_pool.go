ef2efa8efa94def1bbfb19c855fbc8bd1debf1a8
KIMURA Yu <33382781+kimurayu45z@users.noreply.github.com>
2025-04-08 15:15:18 +0900

    msg create pool

--- x/liquiditypool/keeper/msg_server_create_pool.go
+++ x/liquiditypool/keeper/msg_server_create_pool.go
@@ -16,16 +16,31 @@ func (k msgServer) CreatePool(ctx context.Context, msg *types.MsgCreatePool) (*t
 		return nil, errorsmod.Wrap(err, "invalid authority address")
 	}
 
-	// end static validation
+	err := sdk.ValidateDenom(msg.DenomBase)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "invalid denom base")
+	}
 
-	// TODO: enable later after completion of module - for now, disabled for testing
-	// if msg.Authority != k.authority {
-	// 	return nil, types.ErrInvalidSigner
-	// }
+	err = sdk.ValidateDenom(msg.DenomQuote)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "invalid denom quote")
+	}
 
 	feeRate, err := math.LegacyNewDecFromStr(msg.FeeRate)
 	if err != nil {
 		return nil, errorsmod.Wrap(err, "invalid fee rate")
+	}
+
+	if !feeRate.IsPositive() {
+		return nil, errorsmod.Wrap(err, "fee rate must be positive")
+	}
+
+	if feeRate.GTE(math.LegacyOneDec()) {
+		return nil, errorsmod.Wrap(err, "fee rate must be less than 1")
+	}
+
+	if msg.PriceRatio != "1.0001" {
+		return nil, errorsmod.Wrap(err, "price ratio must be 1.0001")
 	}
 
 	priceRatio, err := math.LegacyNewDecFromStr(msg.PriceRatio)
@@ -36,6 +51,25 @@ func (k msgServer) CreatePool(ctx context.Context, msg *types.MsgCreatePool) (*t
 	baseOffset, err := math.LegacyNewDecFromStr(msg.BaseOffset)
 	if err != nil {
 		return nil, errorsmod.Wrap(err, "invalid base offset")
+	}
+
+	if baseOffset.IsPositive() {
+		return nil, errorsmod.Wrap(err, "base offset must be in range (-1, 0]")
+	}
+
+	if baseOffset.LTE(math.LegacyNewDec(-1)) {
+		return nil, errorsmod.Wrap(err, "base offset must be in range (-1, 0]")
+	}
+
+	// end static validation
+
+	params, err := k.Params.Get(ctx)
+	if err != nil {
+		return nil, errorsmod.Wrap(err, "failed to get params")
+	}
+
+	if params.CreatePoolGas > 0 {
+		sdk.UnwrapSDKContext(ctx).GasMeter().ConsumeGas(params.CreatePoolGas, "create pool")
 	}
 
 	var pool = types.Pool{
