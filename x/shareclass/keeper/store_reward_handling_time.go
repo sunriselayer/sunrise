fa129f8a5bf9f86099de8c0e4744e76f12db12fa
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-04-18 15:37:48 +0900

    fix: collection error

--- x/shareclass/keeper/store_reward_handling_time.go
+++ x/shareclass/keeper/store_reward_handling_time.go
@@ -2,22 +2,20 @@ package keeper
 
 import (
 	"context"
+	"errors"
 	"time"
 
+	"cosmossdk.io/collections"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k Keeper) GetLastRewardHandlingTime(ctx context.Context, validatorAddr sdk.ValAddress) (time.Time, error) {
-	exist, err := k.LastRewardHandlingTime.Has(ctx, validatorAddr)
-	if err != nil {
-		return time.Time{}, err
-	}
-	if !exist {
-		// Return default time (Unix time 0)
-		return time.Unix(0, 0), nil
-	}
 	lastRewardHandlingTime, err := k.LastRewardHandlingTime.Get(ctx, validatorAddr)
 	if err != nil {
+		if errors.Is(err, collections.ErrNotFound) {
+			// Return default time (Unix time 0)
+			return time.Unix(0, 0), nil
+		}
 		return time.Time{}, err
 	}
 
