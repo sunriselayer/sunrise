0d8b91973e19a41a3c2d47e17a3a3b8e05b5d0f7
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-04-18 14:40:44 +0900

    ignite generate

--- x/shareclass/types/query.pb.gw.go
+++ x/shareclass/types/query.pb.gw.go
@@ -213,8 +213,8 @@ func local_request_Query_ClaimableRewards_0(ctx context.Context, marshaler runti
 
 }
 
-func request_Query_AddressUnbondings_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryAddressUnbondingsRequest
+func request_Query_AddressUnbonding_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryAddressUnbondingRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -235,13 +235,13 @@ func request_Query_AddressUnbondings_0(ctx context.Context, marshaler runtime.Ma
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "address", err)
 	}
 
-	msg, err := client.AddressUnbondings(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.AddressUnbonding(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Query_AddressUnbondings_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryAddressUnbondingsRequest
+func local_request_Query_AddressUnbonding_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryAddressUnbondingRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -262,7 +262,7 @@ func local_request_Query_AddressUnbondings_0(ctx context.Context, marshaler runt
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "address", err)
 	}
 
-	msg, err := server.AddressUnbondings(ctx, &protoReq)
+	msg, err := server.AddressUnbonding(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -388,7 +388,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 
 	})
 
-	mux.Handle("GET", pattern_Query_AddressUnbondings_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Query_AddressUnbonding_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -399,7 +399,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Query_AddressUnbondings_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Query_AddressUnbonding_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -407,7 +407,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			return
 		}
 
-		forward_Query_AddressUnbondings_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Query_AddressUnbonding_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -552,7 +552,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
-	mux.Handle("GET", pattern_Query_AddressUnbondings_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Query_AddressUnbonding_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -561,14 +561,14 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Query_AddressUnbondings_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Query_AddressUnbonding_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Query_AddressUnbondings_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Query_AddressUnbonding_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -586,7 +586,7 @@ var (
 
 	pattern_Query_ClaimableRewards_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"sunriselayer", "sunrise", "shareclass", "v1", "claimable-rewards"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_AddressUnbondings_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"sunriselayer", "sunrise", "shareclass", "v1", "unbondings", "address"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_AddressUnbonding_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"sunriselayer", "sunrise", "shareclass", "v1", "unbonding", "address"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
@@ -600,5 +600,5 @@ var (
 
 	forward_Query_ClaimableRewards_0 = runtime.ForwardResponseMessage
 
-	forward_Query_AddressUnbondings_0 = runtime.ForwardResponseMessage
+	forward_Query_AddressUnbonding_0 = runtime.ForwardResponseMessage
 )
