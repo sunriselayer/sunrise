953aefdc9ab30ad48eb3dd740fcfe524c56cb030
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-04-18 14:06:38 +0900

    fix: init time

--- x/shareclass/keeper/store_reward_handling_time.go
+++ x/shareclass/keeper/store_reward_handling_time.go
@@ -8,6 +8,14 @@ import (
 )
 
 func (k Keeper) GetLastRewardHandlingTime(ctx context.Context, validatorAddr sdk.ValAddress) (time.Time, error) {
+	exist, err := k.LastRewardHandlingTime.Has(ctx, validatorAddr)
+	if err != nil {
+		return time.Time{}, err
+	}
+	if !exist {
+		// Return default time (Unix time 0)
+		return time.Unix(0, 0), nil
+	}
 	lastRewardHandlingTime, err := k.LastRewardHandlingTime.Get(ctx, validatorAddr)
 	if err != nil {
 		return time.Time{}, err
