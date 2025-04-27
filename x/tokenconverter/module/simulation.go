c6eb7765e2709985452be517649d78a5bcd4ebce
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-04-27 18:40:33 +0800

    remove simulation

--- x/tokenconverter/module/simulation.go
+++ x/tokenconverter/module/simulation.go
@@ -1,47 +1,34 @@
 package tokenconverter
 
 import (
-	"math/rand"
-
-	"github.com/cosmos/cosmos-sdk/simsx"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 
-	"github.com/sunriselayer/sunrise/testutil/sample"
-	"github.com/sunriselayer/sunrise/x/tokenconverter/simulation"
 	"github.com/sunriselayer/sunrise/x/tokenconverter/types"
-)
-
-// avoid unused import issue
-var (
-	_ = rand.Rand{}
-	_ = sample.AccAddress
-	_ = sdk.AccAddress{}
 )
 
 // GenerateGenesisState creates a randomized GenState of the module.
 func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
 	accs := make([]string, len(simState.Accounts))
 	for i, acc := range simState.Accounts {
-		accs[i] = acc.AddressBech32
+		accs[i] = acc.Address.String()
 	}
-	tokenconverterGenesis := types.GenesisState{
+	contracttokenGenesis := types.GenesisState{
 		Params: types.DefaultParams(),
 	}
-	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&tokenconverterGenesis)
+	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&contracttokenGenesis)
 }
 
 // RegisterStoreDecoder registers a decoder.
 func (am AppModule) RegisterStoreDecoder(_ simtypes.StoreDecoderRegistry) {}
 
-// ProposalMsgsX returns msgs used for governance proposals for simulations.
-func (am AppModule) ProposalMsgsX(weights simsx.WeightSource, reg simsx.Registry) {
-	reg.Add(weights.Get("msg_update_params", 100), simulation.MsgUpdateParamsFactory())
+// WeightedOperations returns the all the gov module operations with their respective weights.
+func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+	operations := make([]simtypes.WeightedOperation, 0)
+	return operations
 }
 
-// WeightedOperationsX returns the all the module operations with their respective weights.
-func (am AppModule) WeightedOperationsX(weights simsx.WeightSource, reg simsx.Registry) {
-	reg.Add(weights.Get("msg__convert", 100), simulation.MsgConvertFactory(am.keeper))
-
+// ProposalMsgs returns msgs used for governance proposals for simulations.
+func (am AppModule) ProposalMsgs(simState module.SimulationState) []simtypes.WeightedProposalMsg {
+	return []simtypes.WeightedProposalMsg{}
 }
