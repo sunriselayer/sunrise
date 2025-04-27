2c1de5c96905f8b80d40e20e319aa7252e50b2d6
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-04-27 17:26:01 +0800

    lockup

--- x/lockup/keeper/keeper.go
+++ x/lockup/keeper/keeper.go
@@ -5,7 +5,8 @@ import (
 
 	"cosmossdk.io/collections"
 	"cosmossdk.io/core/address"
-	"cosmossdk.io/core/appmodule"
+	"cosmossdk.io/core/store"
+	"cosmossdk.io/log"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -13,13 +14,15 @@ import (
 )
 
 type Keeper struct {
-	appmodule.Environment
+	cdc          codec.Codec
+	storeService store.KVStoreService
+	logger       log.Logger
 
-	cdc          codec.BinaryCodec
-	addressCodec address.Codec
 	// Address capable of executing a MsgUpdateParams message.
 	// Typically, this should be the x/gov module account.
-	authority []byte
+	authority string
+
+	addressCodec address.Codec
 
 	Schema              collections.Schema
 	Params              collections.Item[types.Params]
@@ -35,37 +38,38 @@ type Keeper struct {
 }
 
 func NewKeeper(
-	env appmodule.Environment,
-	cdc codec.BinaryCodec,
+	cdc codec.Codec,
+	storeService store.KVStoreService,
+	logger log.Logger,
+	authority string,
 	addressCodec address.Codec,
-	authority []byte,
 	accountKeeper types.AccountKeeper,
 	bankKeeper types.BankKeeper,
 	stakingKeeper types.StakingKeeper,
 	feeKeeper types.FeeKeeper,
 	shareclassKeeper types.ShareclassKeeper,
 ) Keeper {
-	if _, err := addressCodec.BytesToString(authority); err != nil {
+	if _, err := addressCodec.StringToBytes(authority); err != nil {
 		panic(fmt.Sprintf("invalid authority address %s: %s", authority, err))
 	}
 
-	sb := collections.NewSchemaBuilder(env.KVStoreService)
+	sb := collections.NewSchemaBuilder(storeService)
 
 	k := Keeper{
-		Environment:  env,
 		cdc:          cdc,
-		addressCodec: addressCodec,
+		storeService: storeService,
+		logger:       logger,
 		authority:    authority,
+		addressCodec: addressCodec,
 
 		Params:              collections.NewItem(sb, types.ParamsKey, "params", codec.CollValue[types.Params](cdc)),
 		NextLockupAccountId: collections.NewMap(sb, types.NextLockupAccountIdKeyPrefix, "next_lockup_account_id", types.NextLockupAccountIdKeyCodec, collections.Uint64Value),
 		LockupAccounts:      collections.NewMap(sb, types.LockupAccountsKeyPrefix, "lockup_accounts", types.LockupAccountsKeyCodec, codec.CollValue[types.LockupAccount](cdc)),
 
-		accountKeeper: accountKeeper,
-		bankKeeper:    bankKeeper,
-		stakingKeeper: stakingKeeper,
-		feeKeeper:     feeKeeper,
-
+		accountKeeper:    accountKeeper,
+		bankKeeper:       bankKeeper,
+		stakingKeeper:    stakingKeeper,
+		feeKeeper:        feeKeeper,
 		shareclassKeeper: shareclassKeeper,
 	}
 
@@ -79,6 +83,11 @@ func NewKeeper(
 }
 
 // GetAuthority returns the module's authority.
-func (k Keeper) GetAuthority() []byte {
+func (k Keeper) GetAuthority() string {
 	return k.authority
+}
+
+// Logger returns a module-specific logger.
+func (k Keeper) Logger() log.Logger {
+	return k.logger.With("module", fmt.Sprintf("x/%s", types.ModuleName))
 }
