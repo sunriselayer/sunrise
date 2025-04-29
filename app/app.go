4b950a5fa9b19e9637b2679beba79c146b1d8cc4
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-04-29 12:59:55 +0800

    module order due to liquidityincentive->shareclass dep

--- app/app.go
+++ app/app.go
@@ -112,11 +112,11 @@ type App struct {
 	DaKeeper                 damodulekeeper.Keeper
 	FeeKeeper                feemodulekeeper.Keeper
 	TokenconverterKeeper     tokenconvertermodulekeeper.Keeper
+	ShareclassKeeper         shareclassmodulekeeper.Keeper
+	LockupKeeper             lockupmodulekeeper.Keeper
 	LiquiditypoolKeeper      liquiditypoolmodulekeeper.Keeper
 	LiquidityincentiveKeeper liquidityincentivemodulekeeper.Keeper
 	SwapKeeper               swapmodulekeeper.Keeper
-	ShareclassKeeper         shareclassmodulekeeper.Keeper
-	LockupKeeper             lockupmodulekeeper.Keeper
 	// this line is used by starport scaffolding # stargate/app/keeperDeclaration
 
 	// simulation manager
@@ -200,11 +200,11 @@ func New(
 		&app.DaKeeper,
 		&app.FeeKeeper,
 		&app.TokenconverterKeeper,
+		&app.ShareclassKeeper,
+		&app.LockupKeeper,
 		&app.LiquiditypoolKeeper,
 		&app.LiquidityincentiveKeeper,
 		&app.SwapKeeper,
-		&app.ShareclassKeeper,
-		&app.LockupKeeper,
 	); err != nil {
 		panic(err)
 	}
