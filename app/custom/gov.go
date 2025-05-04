e89530f9c8ec5e82a9b3b60cd560f1fc5dc25d1e
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-05-05 02:24:33 +0800

    tally

--- app/custom/gov.go
+++ app/custom/gov.go
@@ -33,5 +33,10 @@ func (cm CustomGovModule) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
 	genesis.Params.MaxDepositPeriod = &oneWeek
 	genesis.Params.VotingPeriod = &oneWeek
 
+	// 20.0%
+	// Because we disallow validators to vote with non voting delegators' power,
+	// we need to allow lower quorum.
+	genesis.Params.Quorum = math.LegacyNewDecWithPrec(200, 3).String()
+
 	return cdc.MustMarshalJSON(genesis)
 }
