23571a5f471cb3fd61e51649da604c4a8c3537ce
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-05-13 18:57:10 +0900

    fix: autocli

--- x/liquidityincentive/module/autocli.go
+++ x/liquidityincentive/module/autocli.go
@@ -29,18 +29,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "id"}},
 				},
 				{
-					RpcMethod:      "Gauges",
-					Use:            "all-gauges [previous_epoch_id]",
-					Short:          "List all gauge",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "previous_epoch_id"}},
-				},
-				{
-					RpcMethod:      "Gauge",
-					Use:            "gauge [previous_epoch_id] [pool_id]",
-					Short:          "Shows a gauge",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "previous_epoch_id"}, {ProtoField: "pool_id"}},
-				},
-				{
 					RpcMethod: "Votes",
 					Use:       "all-votes",
 					Short:     "List all gauge votes",
