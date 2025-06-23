ef394288434a8c1ace8e0fa28b1768eef6e3242a
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-06-23 23:52:10 +0900

    fix: tx extension  option

--- x/swap/types/codec.go
+++ x/swap/types/codec.go
@@ -4,6 +4,7 @@ import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
+	"github.com/cosmos/cosmos-sdk/types/tx"
 )
 
 func RegisterInterfaces(registrar codectypes.InterfaceRegistry) {
@@ -15,5 +16,12 @@ func RegisterInterfaces(registrar codectypes.InterfaceRegistry) {
 	registrar.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgUpdateParams{},
 	)
+
+	// Register extension options
+	registrar.RegisterImplementations(
+		(*tx.TxExtensionOptionI)(nil),
+		&SwapBeforeFeeExtension{},
+	)
+
 	msgservice.RegisterMsgServiceDesc(registrar, &_Msg_serviceDesc)
 }
