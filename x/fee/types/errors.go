2327cb9a1abef147766e89139da3fd1c9ca3563e
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-07-03 17:52:54 +0900

    prevent invalid pool set

--- x/fee/types/errors.go
+++ x/fee/types/errors.go
@@ -9,4 +9,5 @@ import (
 // x/fee module sentinel errors
 var (
 	ErrInvalidSigner = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrInvalidPool   = errors.Register(ModuleName, 1101, "invalid pool")
 )
