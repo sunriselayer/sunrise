5a448d5bd198eeee85d263bb6430790c458c5f92
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-07-07 18:56:42 +0900

    call from msg

--- x/da/keeper/keeper_slash.go
+++ x/da/keeper/keeper_slash.go
@@ -2,9 +2,11 @@ package keeper
 
 import (
 	"context"
+	"errors"
 
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 func (k Keeper) GetChallengeCounter(ctx context.Context) uint64 {
@@ -62,26 +64,21 @@ func (k Keeper) DeleteFaultCounter(ctx context.Context, operator sdk.ValAddress)
 }
 
 func (k Keeper) IterateFaultCounters(ctx context.Context,
-	handler func(operator sdk.ValAddress, faultCount uint64) (stop bool),
-) {
-	err := k.FaultCounts.Walk(
+	handler func(operator sdk.ValAddress, faultCount uint64) (stop bool, err error),
+) error {
+	return k.FaultCounts.Walk(
 		ctx,
 		nil,
 		func(key []byte, value uint64) (bool, error) {
-			return handler(key, value), nil
+			return handler(key, value)
 		},
 	)
-	if err != nil {
-		k.Logger().Error(err.Error())
-		return
-	}
 }
 
-func (k Keeper) HandleSlashEpoch(ctx sdk.Context) {
+func (k Keeper) HandleSlashEpoch(ctx sdk.Context) error {
 	params, err := k.Params.Get(ctx)
 	if err != nil {
-		k.Logger().Error(err.Error())
-		return
+		return err
 	}
 	slashFaultThreshold := math.LegacyMustNewDecFromStr(params.SlashFaultThreshold) // TODO: remove with Dec
 	slashFraction := math.LegacyMustNewDecFromStr(params.SlashFraction)             // TODO: remove with Dec
@@ -89,36 +86,37 @@ func (k Keeper) HandleSlashEpoch(ctx sdk.Context) {
 	// reset counter
 	err = k.SetChallengeCounter(ctx, 0)
 	if err != nil {
-		k.Logger().Error(err.Error())
-		return
+		return err
 	}
 	threshold := slashFaultThreshold.MulInt64(int64(challengeCount)).Ceil().TruncateInt().Uint64()
 	powerReduction := k.StakingKeeper.PowerReduction(ctx)
-	k.IterateFaultCounters(ctx, func(operator sdk.ValAddress, faultCount uint64) bool {
+	return k.IterateFaultCounters(ctx, func(operator sdk.ValAddress, faultCount uint64) (bool, error) {
 		validator, err := k.StakingKeeper.Validator(ctx, operator)
 		if err != nil {
-			k.Logger().Error(err.Error())
-			return false
+			if errors.Is(err, stakingtypes.ErrNoValidatorFound) {
+				k.Logger().Info("validator not found during slash epoch, skipping", "validator", operator.String())
+				return false, nil
+			}
+			return false, err
 		}
 
 		defer func() {
 			err := k.DeleteFaultCounter(ctx, operator)
 			if err != nil {
-				k.Logger().Error(err.Error())
+				k.Logger().Error("failed to delete fault counter", "error", err, "validator", operator.String())
 			}
 		}()
 		if validator.IsJailed() || !validator.IsBonded() {
-			return false
+			return false, nil
 		}
 
 		if faultCount <= threshold {
-			return false
+			return false, nil
 		}
 
 		consAddr, err := validator.GetConsAddr()
 		if err != nil {
-			k.Logger().Error(err.Error())
-			return false
+			return false, err
 		}
 
 		err = k.SlashingKeeper.Slash(
@@ -127,14 +125,12 @@ func (k Keeper) HandleSlashEpoch(ctx sdk.Context) {
 			ctx.BlockHeight()-sdk.ValidatorUpdateDelay-1,
 		)
 		if err != nil {
-			k.Logger().Error(err.Error())
-			return false
+			return false, err
 		}
 		err = k.SlashingKeeper.Jail(ctx, consAddr)
 		if err != nil {
-			k.Logger().Error(err.Error())
-			return false
+			return false, err
 		}
-		return false
+		return false, nil
 	})
 }
