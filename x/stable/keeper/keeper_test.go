c9257ac8280d05ad81244969a8ede511a9a64865
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-07-09 21:56:46 +0900

    feat: stable keeper test

--- x/stable/keeper/keeper_test.go
+++ x/stable/keeper/keeper_test.go
@@ -12,16 +12,60 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"go.uber.org/mock/gomock"
 
 	"github.com/sunriselayer/sunrise/x/stable/keeper"
 	module "github.com/sunriselayer/sunrise/x/stable/module"
+	stabletestutil "github.com/sunriselayer/sunrise/x/stable/testutil"
 	"github.com/sunriselayer/sunrise/x/stable/types"
 )
+
+type StableMocks struct {
+	BankKeeper *stabletestutil.MockBankKeeper
+}
+
+func getMocks(t *testing.T) StableMocks {
+	ctrl := gomock.NewController(t)
+
+	return StableMocks{
+		BankKeeper: stabletestutil.NewMockBankKeeper(ctrl),
+	}
+}
+
+func setupMsgServer(t *testing.T) (keeper.Keeper, StableMocks, types.MsgServer, context.Context) {
+	t.Helper()
+
+	encCfg := moduletestutil.MakeTestEncodingConfig(module.AppModule{})
+	addressCodec := addresscodec.NewBech32Codec(sdk.GetConfig().GetBech32AccountAddrPrefix())
+	storeKey := storetypes.NewKVStoreKey(types.StoreKey)
+
+	storeService := runtime.NewKVStoreService(storeKey)
+	ctx := testutil.DefaultContextWithDB(t, storeKey, storetypes.NewTransientStoreKey("transient_test")).Ctx
+
+	authority := authtypes.NewModuleAddress(types.GovModuleName)
+	mocks := getMocks(t)
+
+	k := keeper.NewKeeper(
+		storeService,
+		encCfg.Codec,
+		addressCodec,
+		authority,
+		mocks.BankKeeper,
+	)
+
+	// Initialize params
+	if err := k.Params.Set(ctx, types.DefaultParams()); err != nil {
+		t.Fatalf("failed to set params: %v", err)
+	}
+
+	return k, mocks, keeper.NewMsgServerImpl(k), ctx
+}
 
 type fixture struct {
 	ctx          context.Context
 	keeper       keeper.Keeper
 	addressCodec address.Codec
+	mocks        StableMocks
 }
 
 func initFixture(t *testing.T) *fixture {
@@ -35,13 +79,14 @@ func initFixture(t *testing.T) *fixture {
 	ctx := testutil.DefaultContextWithDB(t, storeKey, storetypes.NewTransientStoreKey("transient_test")).Ctx
 
 	authority := authtypes.NewModuleAddress(types.GovModuleName)
+	mocks := getMocks(t)
 
 	k := keeper.NewKeeper(
 		storeService,
 		encCfg.Codec,
 		addressCodec,
 		authority,
-		nil,
+		mocks.BankKeeper,
 	)
 
 	// Initialize params
@@ -53,5 +98,6 @@ func initFixture(t *testing.T) *fixture {
 		ctx:          ctx,
 		keeper:       k,
 		addressCodec: addressCodec,
+		mocks:        mocks,
 	}
 }
