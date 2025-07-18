48f1d2f2e1adc1eb00509a56a6a5b4a6e57c4ab2
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-07-19 03:07:43 +0900

    feat: new param

--- x/tokenconverter/types/params.go
+++ x/tokenconverter/types/params.go
@@ -1,21 +1,27 @@
 package types
 
 import (
+	fmt "fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/sunriselayer/sunrise/app/consts"
 )
 
 // NewParams creates a new Params instance.
-func NewParams(nonTransferableDenom string, transferableDenom string) Params {
+func NewParams(nonTransferableDenom string, transferableDenom string, allowedAddresses []string) Params {
+	if len(allowedAddresses) == 0 {
+		allowedAddresses = nil
+	}
 	return Params{
 		NonTransferableDenom: nonTransferableDenom,
 		TransferableDenom:    transferableDenom,
+		AllowedAddresses:     allowedAddresses,
 	}
 }
 
 // DefaultParams returns a default set of parameters.
 func DefaultParams() Params {
-	return NewParams(consts.BondDenom, consts.MintDenom)
+	return NewParams(consts.BondDenom, consts.MintDenom, []string{})
 }
 
 // Validate validates the set of params.
@@ -25,6 +31,12 @@ func (p Params) Validate() error {
 	}
 	if err := sdk.ValidateDenom(p.TransferableDenom); err != nil {
 		return err
+	}
+
+	for _, addr := range p.AllowedAddresses {
+		if _, err := sdk.AccAddressFromBech32(addr); err != nil {
+			return fmt.Errorf("invalid address: %w", err)
+		}
 	}
 	return nil
 }
