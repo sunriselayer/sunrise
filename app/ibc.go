7bf246d1b86be337889ffa52989960a2fadbb119
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-09-05 01:46:56 +0900

    feat: IMPORTANT wasm msg server integration

--- app/ibc.go
+++ app/ibc.go
@@ -267,6 +267,10 @@ func (app *App) registerWasmAndIBCModules(appOpts servertypes.AppOptions, nodeCo
 		return err
 	}
 
+	// <sunrise>
+	app.CronKeeper.SetWasmMsgServer(wasmkeeper.NewMsgServerImpl(&app.WasmKeeper))
+	// </sunrise>
+
 	return nil
 }
 
